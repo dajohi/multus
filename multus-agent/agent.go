5fbf9def035ce88ec9a9c2131cfecefa312b8e64
David Hill <dhill@mindcry.org>
2020-04-21 16:47:18 -0400

    agent: add include and exclude support
    if not set, use the default

--- multus-agent/agent.go
+++ multus-agent/agent.go
@@ -24,6 +24,33 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
+	defaultArgs := []string{
+		"--timeout",
+		"10",
+		"--bwlimit",
+		cfg.BWLimit,
+		"-av",
+		"-e",
+		"ssh",
+	}
+	if len(cfg.Includes) == 0 && len(cfg.Excludes) == 0 {
+		defaultArgs = append(defaultArgs, []string{
+			"--include",
+			"**.gz.enc",
+			"--include",
+			"sig.cache",
+			"--exclude",
+			"*",
+		}...)
+	} else {
+		for _, inc := range cfg.Includes {
+			defaultArgs = append(defaultArgs, []string{"--include", inc}...)
+		}
+		for _, exc := range cfg.Excludes {
+			defaultArgs = append(defaultArgs, []string{"--exclude", exc}...)
+		}
+	}
+
 	for _, host := range cfg.Hosts {
 		fmt.Printf("syncing %s\n", host.Hostname)
 		storagePath := filepath.Join(cfg.StoragePath, host.Hostname)
@@ -34,23 +61,10 @@ func main() {
 			continue
 		}
 		backupPath := filepath.Clean(host.BackupPath)
-		args := []string{
-			"--timeout",
-			"10",
-			"--include",
-			"**.gz.enc",
-			"--include",
-			"sig.cache",
-			"--exclude",
-			"*",
-			"--bwlimit",
-			cfg.BWLimit,
-			"-av",
-			"-e",
-			"ssh",
+		args := append(defaultArgs, []string{
 			cfg.Login + "@" + host.Hostname + ":" + filepath.Join(backupPath) + string(os.PathSeparator),
 			storagePath,
-		}
+		}...)
 
 		cmd := exec.CommandContext(ctx, "/usr/local/bin/rsync", args...)
 		fmt.Printf("%s\n", cmd.String())
